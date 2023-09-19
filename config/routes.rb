3e58c7a908e69f64fc7979c1ab8fbd0c871c3496
gayatritntra <gayatri.patil@tntra.io>
2023-09-19 11:13:40 +0530

    added a google-api-client gem in gemfile and other file changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :data_entries
+  root "data_entries#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
